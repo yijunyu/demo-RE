171c8f7c491071117a727aff93be4e434de497a0
Yijun Yu <y.yu@open.ac.uk>
2019-05-11 20:06:53 +0100

    rename MyDsl => Problem

--- xtext-dsl-language-server/build.sh
+++ xtext-dsl-language-server/build.sh
@@ -1,2 +1,10 @@
 rm -rf */src/main/x*
-gradle build && gradlew shadowJar && cd .. && yarn install && cd app && yarn start
+rm -rf io.typefox.xtext.langserver.example/build/classes
+rm -rf io.typefox.xtext.langserver.example/model/generated
+find . -name *MyDsl* | xargs rm -rf 
+gradle build \
+	&& gradlew shadowJar \
+	&& cp $(find . -name Problem.ecore) ../example-workspace/problem/ \
+	&& cp $(find . -name Problem.xtext) ../example-workspace/problem/ \
+	&& cd .. && yarn install \
+	&& cd app && yarn start
